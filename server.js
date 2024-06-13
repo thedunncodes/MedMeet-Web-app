cf6bf51a1edcadb12a2ba1acde40ed41a6ec8405
thedunncodes <tobioluwa348@gmail.com>
2024-06-14 00:32:49 +0100

    Update: Route and server connection

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 // GAteway to the app; Spins up the server
+
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './src/routes';
+
+dotenv.config();
+
+const app = express();
+
+app.use(routes);
+app.use(express.static('public'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log('Server running on port ', Number(port));
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
 // Handles all routes on the web app
+
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+
+const router = Router();
+
+router.get('/', AppController.index);
+
+module.exports = router;
